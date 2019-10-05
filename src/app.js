d4598097950bbba7bbb836760e5662ddfa0666b0
Werner1201 <werner.romling@unigranrio.br>
2019-10-05 12:15:19 -0300

    primeiro commit estrutura basica do projeto

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const routes = require("./routes");
+const express = require("express");
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+module.exports = new App().server;
